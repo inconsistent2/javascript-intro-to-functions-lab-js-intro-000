f39ef4266b42a2272e12fcf67893ca031c3c0844
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-11 11:08:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,13 @@ function logShout(string){
 }
 function logWhisper(string){
   console.log (string.toLowerCase());
-
 }
+function sayHiToGrandma(string){
+  if (string.toUpperCase === uppercase){
+    return "YES INDEED!";
+  }else if (string.toLowerCase === lowercase){
+      return "I can't hear you!";
+  }else if (string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+  }
